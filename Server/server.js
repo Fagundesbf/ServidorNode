da4d1b0923f34cb23161940406995e76e47df956
Fabio B Fagundes <fabinho.bnho@gmail.com>
2018-10-23 14:29:57 -0300

    aulas 3 e 4

--- Server/server.js
+++ Server/server.js
@@ -5,19 +5,58 @@ const debug = require('debug')('server:server');
 const express = require('express');
 
 const app = express();
-const port = 3000;
+// const port = 3000;
+const port = normalizePort(process.env.PORT || '3000');
 app.set('port', port);
 
 const server = http.createServer(app);
 const router = express.Router();
 
-const route = router.get('/', (req, res, next) =>{
+const route = router.get('/', (req, res, next) => {
     res.status(200).send({
-        title:"Node Store API",
+        title: "Node Store API",
         version: "0.0.1"
     });
 });
 app.use('/', route);
 
 server.listen(port);
-console.log('API rodando na porta '+ port);
+console.log('API rodando na porta ' + port);
+
+
+function normalizePort(val) {
+    const port = parseInt(val, 10);
+    if (isNaN(port)) {
+        return val;
+    }
+
+    if (port >= 0) {
+        return port;
+
+    }
+    return false;
+}
+
+function OnError(error) {
+
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+
+    const bind = typeof port === 'string' ?
+        'Pipe ' + port :
+        'Port ' + port;
+
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is alreadey in use');
+            break;
+        default:
+            throw error;
+    }
+
+}
